904d6507b1d128a301666a79323e42f5741e2c1f
Ali Koolivand <alisfp43@gmail.com>
2024-05-13 08:54:08 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import questions from "./data.js";
 
-function App() {
+export default function App() {
+  const [curQuestion, setCurQuestion] = useState(questions[0]);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showPoints, setShowPoints] = useState(false);
+
+  const [points, setPoints] = useState(0);
+
+  const answer = curQuestion.answer;
+  useEffect(() => {
+    if (selectedOption === answer) {
+      setPoints((prevPoints) => prevPoints + 1);
+    }
+  }, [selectedOption, answer]);
+
+  function handleQuestion() {
+    const index = questions.indexOf(curQuestion);
+    if (index === 9) return setShowPoints(true);
+    setCurQuestion(questions[index + 1]);
+    setSelectedOption("");
+  }
+
+  function initializer() {
+    setCurQuestion(questions[0]);
+    setSelectedOption("");
+    setPoints(0);
+    setShowPoints(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      {!showPoints && (
+        <>
+          <p value={curQuestion.id} className="question">
+            {curQuestion.question}
+          </p>
+          <QuestionsList
+            curQuestion={curQuestion}
+            selectedOption={selectedOption}
+            onSelectOption={setSelectedOption}
+            answer={answer}
+          />
+          {selectedOption && <Button onClick={handleQuestion}>Next</Button>}
+        </>
+      )}
+
+      {showPoints && <ScorePage points={points} initializer={initializer} />}
     </div>
   );
 }
 
-export default App;
+function QuestionsList({
+  curQuestion,
+  selectedOption,
+  onSelectOption,
+  answer,
+}) {
+  return (
+    <ul>
+      {curQuestion.options.map((option) => (
+        <li
+          onClick={() => onSelectOption(option.id)}
+          key={option.id}
+          className={`option ${
+            selectedOption
+              ? answer === option.id
+                ? "correct"
+                : "incorrect"
+              : ""
+          } ${option.id === selectedOption ? "selected" : ""}
+          ${selectedOption && "disabled"}
+            `}
+        >
+          {option.text}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick} className="btn">
+      {children}
+    </button>
+  );
+}
+
+function ScorePage({ initializer, points }) {
+  return (
+    <div className="scorePage">
+      <h3>Congratulations</h3>
+      <p>your score is {points}</p>
+      <Button onClick={() => initializer()}>Again</Button>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,108 @@
+const questions = [
+  {
+    id: 1,
+    question:
+      "1) Who sang the title song for the latest Bond film, No Time to Die?",
+    options: [
+      { id: 1, text: "Adele" },
+      { id: 2, text: "Sam Smith" },
+      { id: 3, text: "Billie Eilish" },
+    ],
+    answer: 3,
+  },
+  {
+    id: 2,
+    question:
+      "2) Which flies a green, white, and orange (in that order) tricolor flag?",
+    options: [
+      { id: 1, text: "Ireland" },
+      { id: 2, text: "Ivory Coast" },
+      { id: 3, text: "Italy" },
+    ],
+    answer: 1,
+  },
+  {
+    id: 3,
+    question: "3) What company makes the Xperia model of smartphone?",
+    options: [
+      { id: 1, text: "Samsung" },
+      { id: 2, text: "Sony" },
+      { id: 3, text: "Nokia" },
+    ],
+    answer: 2,
+  },
+  {
+    id: 4,
+    question: "4) Which city is home to the Stuttgart Gate?",
+    options: [
+      { id: 1, text: "Vienna" },
+      { id: 2, text: "Zurich" },
+      { id: 3, text: "Berlin" },
+    ],
+    answer: 3,
+  },
+  {
+    id: 5,
+    question: "5) Which of the following is NOT a fruit?",
+    options: [
+      { id: 1, text: "Rhubarb" },
+      { id: 2, text: "Tomatoes" },
+      { id: 3, text: "Avocados" },
+    ],
+    answer: 1,
+  },
+  {
+    id: 6,
+    question: "6) Where was the first example of paper money used?",
+    options: [
+      { id: 1, text: "China" },
+      { id: 2, text: "Turkey" },
+      { id: 3, text: "Greece" },
+    ],
+    answer: 1,
+  },
+  {
+    id: 7,
+    question: "7) Who is generally considered the inventor of the motor car?",
+    options: [
+      { id: 1, text: "Henry Ford" },
+      { id: 2, text: "Karl Benz" },
+      { id: 3, text: "Henry M. Leland" },
+    ],
+    answer: 2,
+  },
+  {
+    id: 8,
+    question:
+      "8) If you were looking at Iguazu Falls, on what continent would you be?",
+    options: [
+      { id: 1, text: "Asia" },
+      { id: 2, text: "Africa" },
+      { id: 3, text: "South America" },
+    ],
+    answer: 3,
+  },
+  {
+    id: 9,
+    question:
+      "9) What number was the Apollo mission that successfully put a man on the moon for the first time in human history?",
+    options: [
+      { id: 1, text: "Apollo 11" },
+      { id: 2, text: "Apollo 12" },
+      { id: 3, text: "Apollo 13" },
+    ],
+    answer: 1,
+  },
+  {
+    id: 10,
+    question: "10) Which of the following languages has the longest alphabet?",
+    options: [
+      { id: 1, text: "Greek" },
+      { id: 2, text: "Arabic" },
+      { id: 3, text: "Russian" },
+    ],
+    answer: 3,
+  },
+];
+
+export default questions;
